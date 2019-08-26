77e59f4bcaecb1ae6fd7b76c1d9a133cd35c894e
Billy Mackay <bossmack50@gmail.com>
2019-08-26 15:45:23 +0100

    vue is working

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 import Vue from "vue";
 
 document.addEventListener("DOMContentLoaded", () => {
+  console.log("apploaded");
   new Vue({
-    
-  });
+    el: "#app",
+    data: {
+      todos: ["Buy shopping", "Clean bathroom", "Car's MOT"]
+    }
+    });
 })
